6654eefc5ed934adb8f7206c9d462d3d03842303
raienko <raienko.a@gmail.com>
2020-10-23 16:23:27 +0300

    ViewPort minor updates

--- src/screens/World/ViewPort.js
+++ src/screens/World/ViewPort.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {ScrollView} from 'react-native';
 
 export default ({children}) => {
+  const horizontalPlane = useRef();
+  const verticalPlane = useRef();
+
+  const moveCamera = (x, y) => {
+    horizontalPlane.current.scrollTo({x});
+    verticalPlane.current.scrollTo({y});
+  };
+
   return (
-    <ScrollView bounces={false}>
-      <ScrollView bounces={false} horizontal>
+    <ScrollView
+      bounces={false}
+      ref={verticalPlane}
+      scrollEnabled={false}
+      showsVerticalScrollIndicator={false}
+    >
+      <ScrollView
+        horizontal
+        bounces={false}
+        ref={horizontalPlane}
+        scrollEnabled={false}
+        showsHorizontalScrollIndicator={false}>
         {children}
       </ScrollView>
     </ScrollView>
