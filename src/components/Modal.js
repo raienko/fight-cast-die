5d21e99bd733193b70b274a54a4b8458186007a3
Alex Raienko <raienko.a@gmail.com>
2020-10-28 00:41:52 +0200

    rn-units upgraded

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,11 @@ import TranslucentModal from 'react-native-translucent-modal';
 import {doNothing} from 'rn-units';
 
 const Modal = (params) => (
-  <TranslucentModal onRequestClose={doNothing} {...params} />
+  <TranslucentModal
+    onRequestClose={doNothing}
+    supportedOrientations={['portrait', 'landscape']}
+    {...params}
+  />
 );
 
 export default Modal;
--- src/screens/Exploration/Controller.js
+++ src/screens/Exploration/Controller.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useEffect} from 'react';
+import {wait, seconds} from 'rn-units';
 import {useLoading} from './Loading';
 import {useLevel} from './Level';
 
-const Controller = ({children}) => {
-  const {showLoading, hideLoading} = useLoading();
+const Controller = () => {
+  const {hideLoading} = useLoading();
   const {changeLevel} = useLevel();
 
   useEffect(() => {
@@ -12,6 +13,7 @@ const Controller = ({children}) => {
 
   const start = async () => {
     changeLevel('forest_1');
+    await wait(seconds(4));
     hideLoading();
   };
 
